ecaf0c426445ae841cd96653a59f4ccee06f562a
Igor <igoreusttaquio@gmail.com>
2021-01-30 01:23:42 +0000

    Função preencher tabela finalizado

--- js/weasome_cep.js
+++ js/weasome_cep.js
@@ -1,14 +1,13 @@
 ; (function () {
     const cepInput = document.getElementById("cepInput");
     const cepForm = document.getElementById("cepForm");
-    const cepFindButton = document.getElementById("cepFindButton");
-    const cepInfoTable = document.getElementById("cepInfo");
+    const ceptable = document.getElementById("Ceptable");
 
     let url = "https://cep.awesomeapi.com.br/json/";
 
     cepForm.addEventListener("submit", (event) => {
         event.preventDefault();
-        findCep(32210160, url);
+        findCep(cepInput.value, url);
     })
 
     function findCep(cepNumber, url) {
@@ -16,7 +15,7 @@
         url += cepNumber;
         const xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
+            if (this.readyState == 4 && (this.status == 200)) {
                 if (xhttp.responseText) {
                     markTable(xhttp.responseText);
                 }
@@ -25,13 +24,42 @@
 
         try {
             xhttp.open("GET", url, true);
-            xhttp.send();
+            if (xhttp.send().status == 404) {
+                alert("Cep Não encontrado");
+                return;
+            };
         } catch (e) {
             markTable("{}");
         }
     }
 
+    function markTable(cepIfoString) {
+        const cepIfoObject = JSON.parse(cepIfoString);
 
+        if (cepIfoObject == {}) {
+            alert("cep inválido");
+        } else {
+            cepInfoBody.innerHTML = "";
+            for (prop in cepIfoObject) {
+                createTable(prop, cepIfoObject[prop]);
+            }
+        }
+    }
+
+    function createTable(prop, value) {
+
+        tr = document.createElement("tr");
+        tdProp = document.createElement("td");
+        tdValue = document.createElement("td");
+        tdProp.innerText = prop.split('_').join(' ');
+        tdValue.innerText = value;
+
+        tr.appendChild(tdProp);
+        tr.appendChild(tdValue);
+
+        cepInfoBody.appendChild(tr);
+        ceptable.style.visibility = "visible";
+    }
 
 
 })()
